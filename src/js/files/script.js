6dc04fcd6b40a971fed07193d8a9dc8c742d3541
Ivan Mikerin <vmik7@yandex.ru>
2020-09-03 17:18:54 +0300

    Realized nav-menu template + other prettify modifications

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,1 +1,11 @@
-// Your scripts here
+
+// Burger interactive
+
+const menuBurger = document.querySelector('.menu__burger');
+const menuBody = document.querySelector('.menu__body');
+
+menuBurger.addEventListener('click', function(){
+    menuBurger.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+    document.body.classList.toggle('_lock');
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,17 @@
 'use strict'
 
+// JS function to test webp support
 
-// js function to test webp support
 @@include('files/webp-test.js', {})
 
-
 // Dynamic Adapt (https://www.youtube.com/watch?v=QKuMr575vlQ)
+
 @@include('libs/dynamic_adapt/da.js', {})
 
+// Focus-visible (https://www.npmjs.com/package/focus-visible)
 
-// focus-visible (https://www.npmjs.com/package/focus-visible)
 @@include('../../node_modules/focus-visible/dist/focus-visible.js', {})
 
+// Other scripts
 
-// other scripts
 @@include('files/script.js', {})
-
-
-
-let menuBurger = document.querySelector('.menu__burger');
-let menuBody = document.querySelector('.menu__burger');
